57845290c9e7985e50e24440da4ed7926664f382
josh.fn7 <josh.fn7@kakaomobility.com>
2022-06-16 10:18:02 +0900

    [common] Add: Collection.isNotEmpty

---
+++ Common/Extension/Collection+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Collection+Extension.swift
+//  Common
+//
+//  Created by josh.fn7 on 2022/06/16.
+//
+
+import Foundation
+
+public extension Collection {
+    
+    var isNotEmpty: Bool { !isEmpty }
+    
+}
