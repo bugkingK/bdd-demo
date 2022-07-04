db446a1812eafbd7f522be447a740e10f54cd01f
Ki Moon Kwon <33336869+bugkingK@users.noreply.github.com>
2022-07-04 17:14:19 +0900

    Create branch+label.sh

---
+++ scripts/branch+label.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BRANCH_TREE="parking/BUG/improVe/feaTure/rt10"
+SELECTED_TAGS=()
+
+if echo ${BRANCH_TREE} | grep -iqF bug ; then
+    SELECTED_TAGS+=("bug")
+fi
+
+if echo ${BRANCH_TREE} | grep -iqF improve ; then
+    SELECTED_TAGS+=("improve")
+fi
+
+if echo ${BRANCH_TREE} | grep -iqF feature ; then
+    SELECTED_TAGS+=("feature")
+fi
+
+TREE_SPLIT=($(echo $BRANCH_TREE | tr "/" "\n"))
+for (( i=0; i<${#TREE_SPLIT[@]}; i++ )); do
+    if echo ${TREE_SPLIT[i]} | grep -iqF RT ; then
+        SELECTED_TAGS+=( `echo ${TREE_SPLIT[i]} | tr [a-z] [A-Z]` )
+    fi
+done
+
+echo ${SELECTED_TAGS[@]}
